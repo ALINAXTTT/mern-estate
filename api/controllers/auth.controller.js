6a4149379713007b8007dd6fbc58435812fc9655
ALINAXTTT <alinatong0624@gmail.com>
2024-03-09 12:11:38 +0800

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
+const { sign, verify } = jwt;
 
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -8,6 +12,35 @@ export const signUp = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created successfully!");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User Not Found!"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong Credentials!"));
+    }
+    /*
+    If the use is valid. we need to create a unique hash token inside 
+    the browser to contain sign-in situation.
+    secret key: unique for our app
+    */
+    const token = sign({ id: validUser._id }, process.env.JWT_SECRET);
+    //prevent password leaked to the user
+    const { password: pass, ...rest } = validUser._doc;
+    //Save the token as cookie (Session type)
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (err) {
     next(err);
   }
